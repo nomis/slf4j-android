d235195de202a7af6eed1d0c5ce1be4545790e56
Simon Arlott <sa.me.uk>
2013-06-04 07:46:31 +0100

    add documentation to LogLevel

--- src/main/java/eu/lp0/slf4j/android/LogLevel.java
+++ src/main/java/eu/lp0/slf4j/android/LogLevel.java
@@ -22,11 +22,27 @@
  */
 package eu.lp0.slf4j.android;
 
+/**
+ * Configuration of Android logging level.
+ * 
+ * Levels are mapped to the corresponding SLF4J level (except ASSERT which does not exist in SLF4J).
+ */
 public enum LogLevel {
+	/** Suppress all log messages. */
 	SUPPRESS,
+	
+	/** Log messages at ERROR level and above. */
 	ERROR,
+	
+	/** Log messages at WARN level and above. */
 	WARN,
+	
+	/** Log messages at INFO level and above. */
 	INFO,
+	
+	/** Log messages at DEBUG level and above. */
 	DEBUG,
+	
+	/** Log messages at TRACE level and above. */
 	VERBOSE;
 }
